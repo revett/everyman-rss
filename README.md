b7c1a2fbf06eb3cfce4981d85dccdc60cb774d63
Charlie Revett <2796074+revett@users.noreply.github.com>
2021-11-10 22:11:33 +0000

    Vercel (#2)

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ RSS feeds for Everyman Cinema film releases.
 
 I wanted a feed of new film releases within my
 [Inoreader](https://www.inoreader.com) client.
+
+## Usage
+
+### Local
+
+```
+make run
+```
+
+### Vercel
+
+> **Note**: see documentation on
+> [Go runtime](https://vercel.com/docs/runtimes#official-runtimes/go).
+
+Each [`http.HandlerFunc`](https://pkg.go.dev/net/http#HandlerFunc) within `api/`
+is hosted as a separate serverless function.
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
-	"github.com/gorilla/feeds"
 	"github.com/gorilla/mux"
-	"github.com/revett/everyman-rss/pkg/everyman"
+	handler "github.com/revett/everyman-rss/api"
 )
 
 const (
@@ -19,7 +16,7 @@ const (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/films", filmsHandler)
+	r.HandleFunc("/films", handler.Films)
 	http.Handle("/", r)
 
 	srv := &http.Server{
@@ -29,51 +26,9 @@ func main() {
 		ReadTimeout:  timeout * time.Second,
 	}
 
+	log.Println(addr)
+
 	log.Fatal(
 		srv.ListenAndServe(),
 	)
-}
-
-func filmsHandler(w http.ResponseWriter, r *http.Request) {
-	f := feeds.Feed{
-		Title:       "Everyman Cinema - Films",
-		Description: "Latest film releases for Everyman Cinema",
-		Link: &feeds.Link{
-			Href: "https://www.everymancinema.com/film-listings",
-		},
-	}
-
-	c := everyman.NewClient()
-
-	films, err := c.Films()
-	if err != nil {
-		log.Println(err)
-		http.Error(
-			w,
-			"unable to request films from everyman cinema api",
-			http.StatusInternalServerError,
-		)
-	}
-
-	for _, film := range films {
-		f.Add(
-			&feeds.Item{
-				Id:          strconv.Itoa(film.ID),
-				Title:       film.Title,
-				Description: film.Teaser,
-				Link: &feeds.Link{
-					Href: film.URL(),
-				},
-			},
-		)
-	}
-
-	rss, err := f.ToRss()
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "unable to generate rss feed", http.StatusInternalServerError)
-	}
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, rss)
 }
