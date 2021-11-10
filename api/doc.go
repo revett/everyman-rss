9ab11f23efcd77d479ea3a62d508652b261b9b74
revett <2796074+revett@users.noreply.github.com>
2021-11-10 21:56:33 +0000

    Remove doc.go

--- api/doc.go
+++
@@ -1,4 +0,0 @@
-// Package handler (within directory called `api`) is a pattern that is enforced
-// by Vercel for serverless functions using the Go runtime.
-// See: https://vercel.com/docs/runtimes#official-runtimes/go
-package handler
--- api/films.go
+++ api/films.go
@@ -1,3 +1,7 @@
+// Package handler (within directory called `api`) is a pattern that is enforced
+// by Vercel for serverless functions using the Go runtime.
+// See: https://vercel.com/docs/runtimes#official-runtimes/go
+// Note this cannot be within doc.go as Vercel sees that file as an endpoint.
 package handler
 
 import (
