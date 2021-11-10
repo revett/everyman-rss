a54bfd79e37cb6f591c74a7883fad80b508f8a5e
Charlie Revett <2796074+revett@users.noreply.github.com>
2021-11-10 23:09:37 +0000

    Image (#5)

--- api/films.go
+++ api/films.go
@@ -33,25 +33,47 @@ func Films(w http.ResponseWriter, r *http.Request) {
 			"unable to request films from everyman cinema api",
 			http.StatusInternalServerError,
 		)
+		return
 	}
 
 	for _, film := range films {
-		f.Add(
-			&feeds.Item{
-				Id:          strconv.Itoa(film.ID),
-				Title:       film.Title,
-				Description: film.Teaser,
-				Link: &feeds.Link{
-					Href: film.URL(),
-				},
+		// TODO: move to function for FilmToItem
+		i := &feeds.Item{
+			Id:          strconv.Itoa(film.ID),
+			Title:       film.Title,
+			Description: film.Teaser,
+			Link: &feeds.Link{
+				Href: film.URL(),
 			},
-		)
+		}
+
+		if film.HasImage() {
+			length, err := film.ImageLength()
+			if err != nil {
+				log.Println(err)
+				http.Error(
+					w,
+					"unable to calculate image length for rss item",
+					http.StatusInternalServerError,
+				)
+				return
+			}
+
+			i.Enclosure = &feeds.Enclosure{
+				Url:    film.Image(),
+				Type:   film.ImageMIMEType(),
+				Length: strconv.FormatInt(length, 10),
+			}
+		}
+
+		f.Add(i)
 	}
 
 	rss, err := f.ToRss()
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "unable to generate rss feed", http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
