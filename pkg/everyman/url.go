6634630d7891cb4fee11bcbf3ccd24ea93fde1db
Charlie Revett <2796074+revett@users.noreply.github.com>
2021-11-15 19:16:34 +0000

    Everyman Client: Cinemas  (#12)

---
+++ pkg/everyman/url.go
@@ -0,0 +1,9 @@
+package everyman
+
+const (
+	// BaseAPIURL is the URL for the Everyman Cinema API.
+	BaseAPIURL = "https://movieeverymanapi.peachdigital.com"
+
+	// BaseAPIURL is the URL for the Everyman Cinema website.
+	BaseWebURL = "https://everymancinema.com"
+)
