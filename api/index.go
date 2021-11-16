c220fc141bf9ed8465d2bb3ac7fd943de33a08de
revett <2796074+revett@users.noreply.github.com>
2021-11-16 21:19:40 +0000

    Pass data to template within handler

--- api/index.go
+++ api/index.go
@@ -5,15 +5,22 @@
 package handler
 
 import (
+	"context"
 	_ "embed"
 	"net/http"
 	"text/template"
 
 	"github.com/revett/everyman-rss/internal/api"
+	"github.com/revett/everyman-rss/pkg/everyman"
 )
 
 //go:embed template/index.tmpl
 var tmpl string
+
+type templateCinemaValues struct {
+	Name string
+	Slug string
+}
 
 // Index serves a simple HTML page explaining the project.
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +28,33 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	c, err := everyman.NewClientWithResponses(everyman.BaseWebURL)
+	if err != nil {
+		api.InternalServerError(
+			w, err, "unable to create everyman api client",
+		)
+		return
+	}
+
+	cinemas, err := c.CinemasWithResponse(context.TODO())
+	if err != nil {
+		api.InternalServerError(
+			w, err, "unable to request cinemas from everyman cinema api",
+		)
+		return
+	}
+
+	var templateData []templateCinemaValues
+	for _, cinema := range *cinemas.JSON200 {
+		templateData = append(
+			templateData,
+			templateCinemaValues{
+				Name: cinema.CinemaName,
+				Slug: cinema.Slug(),
+			},
+		)
+	}
+
 	t, err := template.New("index").Parse(tmpl)
 	if err != nil {
 		api.InternalServerError(
@@ -29,14 +63,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-
-	err = t.Execute(w, nil)
+	err = t.Execute(w, templateData)
 	if err != nil {
 		api.InternalServerError(
 			w, err, "failed when generating template for page",
 		)
 		return
 	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
 }
