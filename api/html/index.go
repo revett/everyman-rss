4384f5a0c4edaec7e0d561b3be68a9169d814395
revett <2796074+revett@users.noreply.github.com>
2021-11-27 17:34:21 +0000

    Make use of new handler package

--- api/html/index.go
+++ api/html/index.go
@@ -4,39 +4,15 @@
 package html
 
 import (
-	"bytes"
-	"context"
-	_ "embed"
 	"net/http"
-	"text/template"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	commonLog "github.com/revett/common/log"
 	commonMiddleware "github.com/revett/common/middleware"
-	"github.com/revett/everyman-rss/pkg/everyman"
+	"github.com/revett/everyman-rss/internal/handler"
 	"github.com/rs/zerolog/log"
-	"github.com/russross/blackfriday/v2"
 )
-
-var (
-	//go:embed template/README.gen.md
-	readmeMarkdown string
-
-	//go:embed template/index.tmpl
-	indexTemplate string
-)
-
-//go:generate cp ../../README.md template/README.gen.md
-type templateData struct {
-	README  string
-	Cinemas []templateCinemaValues
-}
-
-type templateCinemaValues struct {
-	Name string
-	Slug string
-}
 
 // Index serves a simple HTML page explaining the project.
 func Index(w http.ResponseWriter, r *http.Request) { // nolint:varnamelen
@@ -51,58 +27,6 @@ func Index(w http.ResponseWriter, r *http.Request) { // nolint:varnamelen
 		},
 	))
 
-	e.GET("/*", indexHandler)
+	e.GET("/*", handler.Index)
 	e.ServeHTTP(w, r)
-}
-
-func indexHandler(ctx echo.Context) error {
-	client, err := everyman.NewClientWithResponses(everyman.BaseWebURL)
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError, "unable to create everyman api client",
-		)
-	}
-
-	cinemas, err := client.CinemasWithResponse(context.TODO())
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError,
-			"unable to request cinemas from everyman cinema api",
-		)
-	}
-
-	templateData := templateData{
-		README: string(
-			blackfriday.Run(
-				[]byte(readmeMarkdown),
-			),
-		),
-	}
-
-	for _, cinema := range *cinemas.JSON200 {
-		templateData.Cinemas = append(
-			templateData.Cinemas,
-			templateCinemaValues{
-				Name: cinema.CinemaName,
-				Slug: cinema.Slug(),
-			},
-		)
-	}
-
-	tmpl, err := template.New("index").Parse(indexTemplate)
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError, "failed to parse local template film",
-		)
-	}
-
-	var buf bytes.Buffer
-	err = tmpl.Execute(&buf, templateData)
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError, "failed when generating page template",
-		)
-	}
-
-	return ctx.HTML(http.StatusOK, buf.String())
 }
--- api/rss/films.go
+++ api/rss/films.go
@@ -4,23 +4,14 @@
 package rss
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
-	"github.com/gorilla/feeds"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	commonLog "github.com/revett/common/log"
 	commonMiddleware "github.com/revett/common/middleware"
-	"github.com/revett/everyman-rss/internal/service"
-	"github.com/revett/everyman-rss/pkg/everyman"
+	"github.com/revett/everyman-rss/internal/handler"
 	"github.com/rs/zerolog/log"
-)
-
-const (
-	cacheControl     = "s-maxage=300"
-	cinemaQueryParam = "cinema"
 )
 
 // Films serves an RSS XML feed of the latest film releases from Everyman
@@ -37,91 +28,6 @@ func Films(w http.ResponseWriter, r *http.Request) { // nolint:varnamelen
 		},
 	))
 
-	e.GET("/*", filmsHandler)
+	e.GET("/*", handler.Films)
 	e.ServeHTTP(w, r)
-}
-
-func filmsHandler(ctx echo.Context) error {
-	cinemaSlug := ctx.QueryParam(cinemaQueryParam)
-	if cinemaSlug == "" {
-		m := fmt.Sprintf("request must have '%s' query param", cinemaQueryParam)
-		return echo.NewHTTPError(http.StatusBadRequest, m)
-	}
-
-	cinemasClient, err := everyman.NewClientWithResponses(everyman.BaseWebURL)
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError, "unable to create everyman api client",
-		)
-	}
-
-	cinemas, err := cinemasClient.CinemasWithResponse(context.TODO())
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError,
-			"unable to request cinemas from everyman cinema api",
-		)
-	}
-
-	var cinema everyman.Cinema
-
-	for _, c := range *cinemas.JSON200 {
-		if c.Slug() == cinemaSlug {
-			cinema = c
-			break
-		}
-	}
-
-	if cinema.CinemaId == 0 {
-		m := fmt.Sprintf("cinema '%s' does not exist", cinemaSlug)
-		return echo.NewHTTPError(http.StatusBadRequest, m)
-	}
-
-	filmsClient, err := everyman.NewClientWithResponses(everyman.BaseAPIURL)
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError, "unable to create everyman api client",
-		)
-	}
-
-	films, err := filmsClient.FilmsWithResponse(context.TODO(), cinema.CinemaId)
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError,
-			"unable to request films from everyman cinema api",
-		)
-	}
-
-	feed, err := generateFeed(cinema, *films.JSON200)
-	if err != nil {
-		return echo.NewHTTPError(
-			http.StatusInternalServerError, "unable to generate rss feed",
-		)
-	}
-
-	ctx.Response().Header().Set("Cache-Control", cacheControl)
-	return ctx.Blob(http.StatusOK, "application/xml", []byte(feed))
-}
-
-func generateFeed(cinema everyman.Cinema, films []everyman.Film) (string, error) {
-	feed := feeds.Feed{
-		Title: fmt.Sprintf("Everyman Cinema %s - Films", cinema.CinemaName),
-		Description: fmt.Sprintf(
-			"Latest film releases for Everyman Cinema %s.", cinema.CinemaName,
-		),
-		Link: &feeds.Link{
-			Href: cinema.URL(),
-		},
-	}
-
-	for _, film := range films {
-		feed.Add(service.ConvertEverymanFilmToFeedItem(film))
-	}
-
-	rss, err := feed.ToRss()
-	if err != nil {
-		return "", fmt.Errorf("failed to generate rss feed: %w", err) // nolint:wrapcheck
-	}
-
-	return rss, nil
 }
